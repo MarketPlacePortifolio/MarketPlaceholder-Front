9787b3a0746dbb38f3a7585c74b303ba76f6aad8
AlexandreCSilva <alecosta.silva12@gmail.com>
2023-01-30 12:35:54 -0300

    feat: user context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Reset from './assets/Reset';
+import { ToastContainer } from 'react-toastify';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+import { Login } from './pages/Room';
+import { UserProvider } from './contexts/UserContext';
 
 function App() {
   return (
     <div className='App'>
-      Market 3d
+      <Reset />
+      <ToastContainer />
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route index path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
+      </UserProvider>
     </div>
   );
 }
